436ba9ad48c276e32e0dd8eb99fbf1aa264d021c
jhj2075 <flg989@naver.com>
2024-06-23 16:47:14 +0900

    fix: urls

--- todoApp/urls.py
+++ todoApp/urls.py
@@ -19,5 +19,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("/<int:user_id>", views.Todos.as_view())
+    path("<int:user_id>", views.Todos.as_view())
 ]
--- todolist/urls.py
+++ todolist/urls.py
@@ -19,6 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/users', include('userApp.urls')),
-    path('api/todos', include('todoApp.urls')),
+    path('api/users/', include('userApp.urls')),
+    path('api/todos/', include('todoApp.urls')),
 ]
